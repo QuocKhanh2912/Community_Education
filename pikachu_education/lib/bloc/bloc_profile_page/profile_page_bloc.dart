95461836055b4465cdc0436831a40b7556c9d9ca
Quoc Khanh <quockhanh2912.nguyen@gmail.com>
2023-08-14 19:44:41 +0700

    fix import

--- pikachu_education/lib/bloc/bloc_profile_page/profile_page_bloc.dart
+++ pikachu_education/lib/bloc/bloc_profile_page/profile_page_bloc.dart
@@ -1,14 +1,9 @@
-
 import 'dart:io';
-
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:pikachu_education/service/database_service/database_service.dart';
-
-import '../../service/storage_service/storage_service.dart';
-
+import 'package:pikachu_education/service/storage_service/storage_service.dart';
 part 'profile_page_event.dart';
-
 part 'profile_page_state.dart';
 
 class ProfilePageBloc extends Bloc<ProfilePageEvent, ProfilePageState> {
@@ -18,7 +13,7 @@ class ProfilePageBloc extends Bloc<ProfilePageEvent, ProfilePageState> {
     });
 
     on<PostAvatarEvent>((event, emit) async {
-     // await StorageService.deleteImageOnStorage(imageName: event.currentImageName);
+      // await StorageService.deleteImageOnStorage(imageName: event.currentImageName);
       var avatarUrl =
           await StorageService.upLoadImageToStorage(file: event.file);
       await DatabaseService.postUserAvatar(
